eaa24b47c06246e177f31f0ee56c9fbcce7b2955
ZaXandr <Zakharov_Alex18@outlook.com>
2022-02-10 23:29:21 +0200

    add global exception handler

---
+++ src/main/java/com/cursor/team2/movieproject/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.cursor.team2.movieproject.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
