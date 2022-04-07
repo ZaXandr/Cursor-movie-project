ea1de142187f59985e6747f06487b5d84a10ccdb
ZaXandr <Zakharov_Alex18@outlook.com>
2022-04-07 09:38:06 +0300

    modify service

--- src/main/java/com/cursor/team2/movieproject/service/MovieService.java
+++ src/main/java/com/cursor/team2/movieproject/service/MovieService.java
@@ -2,112 +2,122 @@ package com.cursor.team2.movieproject.service;
 
 import com.cursor.team2.movieproject.dto.MovieDto;
 import com.cursor.team2.movieproject.dto.MoviePageDto;
-import com.cursor.team2.movieproject.dto.ReviewDto;
 import com.cursor.team2.movieproject.entity.Category;
 import com.cursor.team2.movieproject.entity.Movie;
 import com.cursor.team2.movieproject.entity.Rate;
+import com.cursor.team2.movieproject.exception.*;
 import com.cursor.team2.movieproject.mapper.MovieMapper;
 import com.cursor.team2.movieproject.mapper.ReviewMapper;
 import com.cursor.team2.movieproject.repository.MovieRepository;
 import com.cursor.team2.movieproject.repository.RateRepository;
 import com.cursor.team2.movieproject.repository.ReviewRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
 
-    @Autowired
-    MovieRepository movieRepository;
 
-    @Autowired
-    ReviewRepository reviewRepository;
+    private final MovieRepository movieRepository;
 
-    @Autowired
-    RateRepository rateRepository;
+    private final ReviewRepository reviewRepository;
 
-    @Autowired
-    MovieMapper movieMapper;
+    private final RateRepository rateRepository;
 
-    @Autowired
-    ReviewMapper reviewMapper;
+    private final MovieMapper movieMapper;
+
+    private final ReviewMapper reviewMapper;
+
+    public MovieService(MovieRepository movieRepository, ReviewRepository reviewRepository, RateRepository rateRepository, MovieMapper movieMapper, ReviewMapper reviewMapper) {
+        this.movieRepository = movieRepository;
+        this.reviewRepository = reviewRepository;
+        this.rateRepository = rateRepository;
+        this.movieMapper = movieMapper;
+        this.reviewMapper = reviewMapper;
+    }
 
 
     public List<MovieDto> getAllMovie() {
-        return movieRepository.findAll().stream().map(movie -> {return movieMapper.toDto(movie);}).collect(Collectors.toList());
+        return movieRepository.findAll().stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     public MoviePageDto getMovieById(long id) {
-        MoviePageDto movie = new MoviePageDto();
-        movie = movieMapper.toPageDto(movieRepository.getById(id)).setReviews(reviewRepository.getByMovieId(id)
-                .stream()
-                .map(review -> {
-                    ReviewDto reviewDto = reviewMapper.toDto(review);
-                    return reviewDto;
-                })
-                .collect(Collectors.toList()));
-
-        return movie;
+        return movieRepository.findById(id)
+                .map(movieMapper::toPageDto)
+                .map(movieDto -> movieDto.setReviews(
+                    reviewRepository.geByMovieId(id).stream()
+                            .map(reviewMapper::toDto)
+                            .collect(Collectors.toList()))
+                )
+                .orElseThrow(() -> new MovieNotFoundException(id));
     }
 
     public Movie addMovie(Movie movie) {
-        Optional<Movie> movieById = movieRepository.findById(movie.getId());
-        if (movieById.isEmpty()) {
-            return movieRepository.save(movie);
+        if (movieRepository.existsById(movie.getId())) {
+            throw new MovieAlreadyExistsException(movie.getId());
         } else {
-//            throw new MovieAlreadyExistsException();
-            return null;
+            return movieRepository.save(movie);
         }
     }
 
     public Movie updateMovie(Movie movie) {
-        Optional<Movie> movieById = movieRepository.findById(movie.getId());
-        if (movieById.isEmpty()) {
-//            throw new NotFoundMovieException(message);
-            return null;
-        } else {
+        if (movieRepository.existsById(movie.getId())) {
             return movieRepository.save(movie);
+        } else {
+            throw new MovieNotFoundException(movie.getId());
         }
     }
 
     public void deleteMovie(long id) {
-        Movie movie = movieRepository.getById(id);
-        if (Objects.isNull(movie)) {
-// throw new NotFoundMovieException(message);
+        if (movieRepository.existsById(id)) {
+            movieRepository.deleteById(id);
         } else {
-            movieRepository.delete(movie);
+            throw new MovieNotFoundException(id);
         }
     }
 
-    public Rate updateRating(int value) {
-        Movie movie = movieRepository.getById(1);
-        Rate rate = movie.getRate();
-        rate.setAverage((movie.getRate().getAverage() * movie.getRate().getVoices() + value) / (movie.getRate().getVoices()+1));
-        rate.setVoices(rate.getVoices()+1);
-        return rateRepository.save(rate);
-
+    public Rate updateRating(Long id, int value) {
+        return movieRepository.findById(id)
+                .map(movie -> {
+                    if (value > 10 || value < 0) {
+                        throw new WrongRatingException("unable to put value:", value);
+                    } else {
+                        Rate rate = movie.getRate();
+                        rate.setAverage((movie.getRate().getAverage() * movie.getRate().getVoices() + value)
+                                / (movie.getRate().getVoices() + 1));
+                        rate.setVoices(rate.getVoices() + 1);
+                        return rateRepository.save(rate);
+                    }
+                })
+                .orElseThrow(() -> new MovieNotFoundException(id));
     }
 
     public List<MovieDto> getMovieByCategory(String category) {
         Category categoryForSearch = Category.testByKey(category);
-        return movieRepository.findByCategory(categoryForSearch).stream().
-                map(movie ->
-                {MovieDto movieDto = movieMapper.toDto(movie);
-                    return movieDto;})
+        if (Objects.isNull(categoryForSearch)) {
+            throw new WrongCategoryException("Wrong category name: ", category);
+        } else {
+            return movieRepository.findByCategory(categoryForSearch).stream()
+                    .map(movieMapper::toDto)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public List<MovieDto> getMovieByRatingDesc() {
+        return movieRepository.getMovieByRatingDesc().stream()
+                .map(movieMapper::toDto)
                 .collect(Collectors.toList());
     }
 
-    public List<MovieDto> getMovieByRating() {
-        return movieRepository.getMovieByRating().stream()
-                .map(movie -> {
-                    MovieDto movieDto = movieMapper.toDto(movie);
-                    return movieDto;
-                })
+
+    public List<MovieDto> getMovieByRatingAsc() {
+        return movieRepository.getMovieByRatingAsc().stream()
+                .map(movieMapper::toDto)
                 .collect(Collectors.toList());
     }
 }
